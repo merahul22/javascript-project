d66e7f35f49b593f9986513d622ce4af24b417d6
Rahul chourasiya <130483145+merahul22@users.noreply.github.com>
2024-04-09 18:12:36 +0000

    javascript remaining in project 1

---
+++ project1/script.js
@@ -0,0 +1,26 @@
+  const box=document.querySelectorAll('#box')
+const body=document.querySelector('body')
+
+box.forEach(button => {
+
+  button.addEventListener('click', function (e) {
+    console.log(e);
+    console.log(e.target);
+    if (e.target.className === 'grey') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.className === 'white') {
+      body.style.backgroundColor = e.target.className;
+    }
+    if (e.target.className === 'red') {
+      body.style.backgroundColor = e.target.className;
+    }
+    if (e.target.className === 'green') {
+      body.style.backgroundColor = e.target.className;
+    }
+
+
+    
+});
+})
+
